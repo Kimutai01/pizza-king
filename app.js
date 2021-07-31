827361c8c869ef7448b371e5de8b67f28c231362
Kimutai01 <kiprotichkimutai01@gmail.com>
2021-07-31 09:32:06 -0400

    creating size functions

--- app.js
+++ app.js
@@ -10,16 +10,68 @@ function typeDeluxe(){
  }
 
  function typePepperoni(){
-    let pepperoniType = document.getElementById('pepperoni').value 
-     document.getElementById('result').innerHTML = 'Chicken & beef pepperoni'
+    let pepperoniType = document.getElementById('pepperoni').value;
+     document.getElementById('result').innerHTML = 'Chicken & beef pepperoni';
  }
 
  function typeVeg(){
-    let pvegType = document.getElementById('veg').value 
-     document.getElementById('result').innerHTML = 'veg Feast'
+    let vegType = document.getElementById('veg').value ;
+     document.getElementById('result').innerHTML = 'veg Feast';
  }
 
 //  pizza-size
+
+function largePizza (){
+    let pizzaLarge = document.getElementById('large').value;
+    document.getElementById('size-result').innerHTML = 'Large'
+}
+
+function mediumPizza (){
+    let pizzaMedium= document.getElementById('medium').value;
+    document.getElementById('size-result').innerHTML = 'medium'
+}
+
+function smallPizza (){
+    let pizzaSmall = document.getElementById('small').value;
+    document.getElementById('size-result').innerHTML = 'small'
+}
+
+// crust
+
+function stuffedCrust (){
+    let crustStuffed = document.getElementById('stuffed').value;
+    document.getElementById('crust-type').innerHTML = 'Stuffed Crust'
+}
+
+function crispyCrust (){
+    let crustCrispy = document.getElementById('crispy').value;
+    document.getElementById('crust-type').innerHTML = 'Crispy Crust'
+}
+
+function gluttenCrust (){
+    let crustGlutten = document.getElementById('glutten').value;
+    document.getElementById('crust-type').innerHTML = 'Glutten Crust'
+}
+// topping
+
+function sausageToping(){
+    let topingSausage = document.getElementById('sausage').value;
+    document.getElementById('toping-type').innerHTML = 'sausage toping'
+}
+
+function meatToping (){
+    let topingMeat = document.getElementById('meat').value;
+    document.getElementById('toping-type').innerHTML = 'meat toping'
+}
+function onionToping (){
+    let topingOnion = document.getElementById('onion').value;
+    document.getElementById('toping-type').innerHTML = 'onion toping'
+}
+function cheeseToping (){
+    let topingCheese = document.getElementById('cheese').value;
+    document.getElementById('toping-type').innerHTML = 'cheese toping'
+}
+
 
 
 
