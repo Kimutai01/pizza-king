b0bda30c85002aa52e722bddb070391ba666f6ba
Kimutai01 <kiprotichkimutai01@gmail.com>
2021-08-01 12:46:15 -0400

    adding the error messages

--- app.js
+++ app.js
@@ -148,13 +148,88 @@ function thePrice(){
     let sum= (parseInt(document.getElementById('size-price').innerHTML)+parseInt(document.getElementById('toping-price').innerHTML) + parseInt( document.getElementById('crust-price').innerHTML))
     let totalSum= sum * document.getElementById('num3').innerHTML
     document.getElementById('total-price').innerHTML = totalSum
+    
 
 
 }
 
 function withoutDelivery(){
-    alert('Your order has been recieved for ' + ' ' + document.getElementById('num3').innerHTML + ' ' +  document.getElementById('result').innerHTML+ ' ' + document.getElementById('size-result').innerHTML + ' ' + document.getElementById('crust-type').innerHTML + ' ' + ' with ' + ' ' + document.getElementById('toping-type').innerHTML + ' and the total price is ' + ' ' + document.getElementById('total-price').innerHTML)
+
+    swal({
+        title: "order recieved",
+        text: 'Your order has been recieved for ' + ' ' + document.getElementById('num3').innerHTML + ' ' +  document.getElementById('result').innerHTML+ ' ' + document.getElementById('size-result').innerHTML + ' ' + document.getElementById('crust-type').innerHTML + ' ' + ' with ' + ' ' + document.getElementById('toping-type').innerHTML + ' and the total price is ' + ' ' + document.getElementById('total-price').innerHTML + ' ' + 'you will recieve your order in 15 minutes',
+        icon: "success",
+      });
 }
+
+function withDelivery(){
+  swal('Delivery cost is 250 shillings')
+  document.getElementById('delivery-fee').innerHTML = 'The delivery price is' + 250
+  let myLocation = prompt('what is your location');
+  let phoneNo = prompt('Enter your phone number')
+  document.getElementById('info').innerHTML= 'Your Location is' + ' ' + myLocation
+  document.getElementById('phone').innerHTML=  'Your phone number is ' + ' ' + phoneNo
+  swal('view your price list below');
+
+  let grandTotal = parseInt(document.getElementById('total-price').innerHTML) + parseInt(250);
+
+  document.getElementById('grand').innerHTML= 'your grand total is ' + ' ' + grandTotal
+
+  document.getElementById('every').innerHTML = 'The total amount together with delivery fee is ' +' ' + grandTotal + ' ' + 'your order will be delivered to ' + ' ' +  myLocation + ' ' + 'within 30 minutes' + ' ' + 'you will be contacted through' + ' ' + phoneNo + ' ' + 'when the pizza is delivered'
+
+if(myLocation=='' && phoneNo==''){
+   swal('please enter location and pone number');
+   document.getElementById('grand').innerHTML=''
+   document.getElementById('every').innerHTML=''
+   document.getElementById('info').innerHTML=''
+   document.getElementById('phone').innerHTML=''
+   document.getElementById('delivery-fee').innerHTML=''
+
+}else if(myLocation==''){
+    swal('please enter location');
+    document.getElementById('grand').innerHTML=''
+    document.getElementById('every').innerHTML=''
+    document.getElementById('info').innerHTML=''
+    document.getElementById('phone').innerHTML=''
+    document.getElementById('delivery-fee').innerHTML=''
+}else if(phoneNo==''){
+    swal('please enter phone number')
+    document.getElementById('grand').innerHTML=''
+    document.getElementById('every').innerHTML=''
+    document.getElementById('info').innerHTML=''
+    document.getElementById('phone').innerHTML=''
+    document.getElementById('delivery-fee').innerHTML=''
+}else if(myLocation==null && phoneNo==null){
+    swal('please enter location and pone number');
+    document.getElementById('grand').innerHTML=''
+    document.getElementById('every').innerHTML=''
+    document.getElementById('info').innerHTML=''
+    document.getElementById('phone').innerHTML=''
+    document.getElementById('delivery-fee').innerHTML=''
+ 
+}else if (myLocation==null){
+    swal('please enter location');
+    document.getElementById('grand').innerHTML=''
+    document.getElementById('every').innerHTML=''
+    document.getElementById('info').innerHTML=''
+    document.getElementById('phone').innerHTML=''
+    document.getElementById('delivery-fee').innerHTML=''
+}else if(phoneNo==null){
+    swal('please enter phone number')
+    document.getElementById('grand').innerHTML=''
+    document.getElementById('every').innerHTML=''
+    document.getElementById('info').innerHTML=''
+    document.getElementById('phone').innerHTML=''
+    document.getElementById('delivery-fee').innerHTML=''
+}
+
+
+}
+
+
+
+
+
 
 
 
