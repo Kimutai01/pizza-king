eff02c1c5a4ed3b2a4134c2aabc048382b64e38f
Kimutai01 <kiprotichkimutai01@gmail.com>
2021-08-01 06:12:22 -0400

    working on toppings

--- jquery.js
+++ jquery.js
@@ -1,9 +1,6 @@
-function Contact(name, phone, location){
-    this.name = name;
-    this.phone = phone;
-    this.location = location;
-}
 
 $(document).ready(function(){
-    
+    $('.btn1').click(function(){
+        $('.resu').fadeIn()
+    })
 })
